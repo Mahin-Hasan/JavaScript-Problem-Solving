355eb4a0278a991f018b461f372119a6320a0b1b
Mahin Hasan <mahinmtrs@gmail.com>
2023-09-25 18:04:32 +0600

    Nesting the Object with array of objects and function

--- object.js
+++ object.js
@@ -4,10 +4,22 @@ const student = {
     address: 'Sylhet, Bangladesh',
     isSingle: true,
     friends: ['Mahdy', 'Mehat', 'Adia'],
+    // An object inside an Array
+    movies: [{ name: 'kgf', year: 2018 }, { name: 'jawan', year: 2023 }],
+    // function inside an Object
+    act: function(){
+        console.log('Actor is Acting');
+    },
+    // An object inside object
     car: {
         brand: 'Toyota',
         price: 170000,
-        made: 2003
+        made: 2003,
+        manufacturer: {
+            ceo: 'Toyoda',
+            country: 'Japan'
+        }
     }
 }
 console.log(student);
+student.act();
