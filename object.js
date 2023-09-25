5d334ffbc8eeef5bbfe40cb1bc6ecdd5b43d73c0
Mahin Hasan <mahinmtrs@gmail.com>
2023-09-25 17:56:23 +0600

    Object inside an object

---
+++ object.js
@@ -0,0 +1,13 @@
+const student = {
+    name: 'Mahin Hasan',
+    id: 121,
+    address: 'Sylhet, Bangladesh',
+    isSingle: true,
+    friends: ['Mahdy', 'Mehat', 'Adia'],
+    car: {
+        brand: 'Toyota',
+        price: 170000,
+        made: 2003
+    }
+}
+console.log(student);
